c09b57294388e8e9f6f11b76f18cc5daa23a567d
Senbongi <suzunenosebun@gmail.com>
2016-10-28 23:12:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<title>こちらはLINE Messaging APIのデモサイトです。</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+こちらはLINE Messaging APIのデモサイトです。
+</body>
+</html>
+Contact GitHub API Training Shop Blog About
